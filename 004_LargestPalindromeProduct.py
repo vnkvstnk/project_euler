2f856986935eef2991e9272561921b13c928b26e
Ivan <44145859+vnkvstnk@users.noreply.github.com>
2018-11-18 23:41:22 +0300

    Minor changes

--- 004_LargestPalindromeProduct.py
+++ 004_LargestPalindromeProduct.py
@@ -1,7 +1,4 @@
 import logging
-from timeit import default_timer as timer
-logging.basicConfig(level='DEBUG')
-logging.disable()
 # function to check if number is palindrome
 def isPalindrome(num):
     initNum = num       # saving the initial number
@@ -11,9 +8,7 @@ def isPalindrome(num):
         reverse = (reverse * 10) + lastDigit
         num = num // 10
     return reverse == initNum
-logging.debug(isPalindrome(121))
 
-start = timer()
 # looking for palindromes in products of numbers between 900 and 999
 palindromes = []
 multipliers = range(900, 1000)
@@ -22,10 +17,5 @@ for i in range(0, len(multipliers)):
         potentialPalindrome = multipliers[i] * multipliers [j]
         if isPalindrome(potentialPalindrome):
             palindromes.append(potentialPalindrome)
-logging.debug(palindromes)
-end = timer()
 print(palindromes)
 print('The maximum palindrome is %s' % max(palindromes))
-print('Ececution time - %s s' % (end - start))
-
-
