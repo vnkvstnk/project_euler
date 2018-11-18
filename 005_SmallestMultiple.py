2e5c7af15ece09137c115dd71a0d34ceb70c4706
Ivan <44145859+vnkvstnk@users.noreply.github.com>
2018-11-18 23:42:35 +0300

    Minor changes

--- 005_SmallestMultiple.py
+++ 005_SmallestMultiple.py
@@ -1,13 +1,12 @@
 #! python3
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-def isDevisable(num):
+def isDivisible(num):
     if num % 20 == 0 and num % 19 == 0 and num % 17 == 0 and num % 16 == 0 \
         and num % 13 == 0 and num % 11 == 0 and num % 9 == 0 and num % 8 == 0 \
         and num % 7 == 0 and num % 4 == 0 and num % 3 == 0:
         return True
 
 i = 2520
-while not isDevisable(i):
+while not isDivisible(i):
     i += 20
 print(i)
-
