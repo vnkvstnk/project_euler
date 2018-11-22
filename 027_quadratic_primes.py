e76fe793d00b21f7a3b11e789a431e1131a9376b
Ivan Tiunov <tiunov.i@gubkin.ru>
2018-11-22 04:00:07 +0300

    Problem # 27. Quadratic primes / v1

---
+++ 027_quadratic_primes.py
@@ -0,0 +1,9 @@
+# Project Euler, problem #27
+# Considering quadratics of the form:
+#     n^2+an+b, where |a|<1000 and |b|≤1000
+# where |n| is the modulus/absolute value of n
+# e.g. |11| = 11 and |−4| = 4
+# Find the product of the coefficients, a and b, for the quadratic expression
+# that produces the maximum number of primes for consecutive values of n, starting with n = 0.
+
+# Chapter 4, Prime-Producing Polynomials, in Ribenboim's book, The New Book of Prime Number Records
